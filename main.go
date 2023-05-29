81cbce3f0d29cdc41afb6b7b9cf0174439278a7e
Damien MARGUERITE <damien.marguerite@pm.me>
2023-05-29 16:39:09 +0200

    Add a command line parser.

--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"github.com/mstch/gotrack/provider"
 )
 
 func main() {
-	request := provider.NewRequest("laposte", "LU680211095FR")
+	// Define command-line flags
+	tracker := flag.String("p", "", "Tracker")
+	packageID := flag.String("i", "", "Package ID")
+
+	// Parse command-line arguments
+	flag.Parse()
+
+	// Check if the required flags are provided
+	if *tracker == "" || *packageID == "" {
+		fmt.Println("Usage: gotracker -p [TRACKER] -i [PACKAGE_ID]")
+		return
+	}
+
+	request := provider.NewRequest(*tracker, *packageID)
 
 	fmt.Println(request)
 }
