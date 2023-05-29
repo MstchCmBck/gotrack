bccc962ace0735dce7855587b01064b1c7a80089
Damien MARGUERITE <damien.marguerite@pm.me>
2023-05-29 18:41:00 +0200

    Move Command Line parsing into it's one source

--- main.go
+++ main.go
@@ -2,25 +2,16 @@ package main
 
 import (
 	"fmt"
-	"flag"
+	"os"
 	"github.com/mstch/gotrack/provider"
+	"github.com/mstch/gotrack/utils/commandline"
 )
 
 func main() {
-	// Define command-line flags
-	tracker := flag.String("p", "", "Tracker")
-	packageID := flag.String("i", "", "Package ID")
+	var args commandline.Args
+	args.Parse(os.Args[1:])
 
-	// Parse command-line arguments
-	flag.Parse()
-
-	// Check if the required flags are provided
-	if *tracker == "" || *packageID == "" {
-		fmt.Println("Usage: gotracker -p [TRACKER] -i [PACKAGE_ID]")
-		return
-	}
-
-	request := provider.NewRequest(*tracker, *packageID)
+	request := provider.NewRequest(args.GetTracker(), args.GetPackageId())
 
 	fmt.Println(request)
 }
---
+++ utils/commandline/commandline.go
@@ -0,0 +1,40 @@
+package commandline
+
+import (
+	"flag"
+	"fmt"
+)
+
+type Args struct {
+	tracker string
+	packageId string
+}
+
+func (a *Args) Parse(args []string) {
+	fmt.Println(args)
+
+	// Define command-line flags
+	tracker := flag.String("p", "", "Tracker")
+	packageId := flag.String("i", "", "Package ID")
+
+	// Parse command-line arguments
+	flag.CommandLine.Parse(args)
+
+	// Fill Args struct
+	a.tracker = *tracker
+	a.packageId = *packageId
+
+	// Check if the required flags are provided
+	if a.tracker == "" || a.packageId == "" {
+		fmt.Println("Usage: gotracker -p [TRACKER] -i [PACKAGE_ID]")
+		return
+	}
+}
+
+func (a Args) GetTracker() string {
+	return a.tracker
+}
+
+func (a Args) GetPackageId() string {
+	return a.packageId
+}
